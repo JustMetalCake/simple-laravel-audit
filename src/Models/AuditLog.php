1dcbd6dfb324cae4d8bb95f0e84f63ae254b8756
Charlie Johnson <charlie.johnson@motocom.co.uk>
2026-08-13 17:32:26 +0100

    Model WIP

--- src/Models/AuditLog.php
+++ src/Models/AuditLog.php
@@ -2,7 +2,11 @@
 
 namespace Motomedialab\SimpleLaravelAudit\Models;
 
+use App\Models\User;
+use Filament\Facades\Filament;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class AuditLog extends Model
 {
@@ -20,5 +24,43 @@ class AuditLog extends Model
     public function getTable()
     {
         return config('simple-auditor.table_name', 'audit_logs');
+    }
+
+    public function hasAuditableModel(): Attribute
+    {
+        return Attribute::get(fn () => collect($this->context)->has(['id', 'class']));
+    }
+
+    public function model(): ?array
+    {
+        if (!$this->hasAuditableModel) {
+            return null;
+        }
+
+        /** @var ?string $class */
+        $class = collect($this->context)->get('class');
+
+        if ($class && class_exists($class)) {
+            return [
+                'id' => collect($this->context)->get('id'),
+                'class' => $class,
+            ];
+        }
+
+        return null;
+    }
+
+    public function filamentResource(): ?string
+    {
+        if ($this->model()) {
+            return Filament::getModelResource($this->model()['class']);
+        }
+
+        return null;
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
     }
 }
